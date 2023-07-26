04ba21dc2291b791acf5eaa3e9e37dc206e0aa2d
carinojake <lusir999@gmail.com>
2023-07-26 16:32:11 +0700

    con

---
+++ database/migrations/2023_07_26_153343_add_task_po_er_date.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+
+            $table->timestamp('task_po_start_date')->nullable();
+            $table->timestamp('task_po_end_date')->nullable();
+            $table->timestamp('task_er_start_date')->nullable();
+            $table->timestamp('task_er_end_date')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('tasks', function (Blueprint $table) {
+            //
+        });
+    }
+};
