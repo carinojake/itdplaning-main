0e7ec4b728461aa5cb77a6a4bbc40be865697296
carinojake <lusir999@gmail.com>
2023-08-04 15:12:17 +0700

    contract_has_taskcons migr

---
+++ database/migrations/2023_08_04_150632_contract_has_taskscon.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ContractHasTaskconTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('contract_has_taskscon', function (Blueprint $table) {
+            $table->unsignedBigInteger('contract_id');
+            $table->unsignedBigInteger('task_id');
+            $table->index('contract_id', 'contract_has_taskscon');
+
+            // $table->foreign('contract_id')
+            //     ->references('task_id')
+            //     ->on('tasks')
+            //     ->onDelete('cascade');
+        });
+
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contract_has_taskcons');
+        //
+    }
+};
